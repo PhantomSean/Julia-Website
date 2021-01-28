282b16a2d114788f5049599897208a96255f6acf
PhantomSean <sean.stewart@ucdconnect.ie>
2021-01-28 11:10:15 +0000

    Changed routing for admin and home. Removed epilogue dependency and replaced with finale-rest to fix depreciation issue

--- blog/index.js
+++ blog/index.js
@@ -5,11 +5,12 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const { ExpressOIDC } = require('@okta/oidc-middleware');
+const Sequelize = require('sequelize');
+const finale = require('finale-rest'), ForbiddenError = finale.Errors.ForbiddenError;
 const app = express();
 const port = 3000;
 
 // session support is required to use ExpressOIDC
-
 app.use(session({
     secret: process.env.RANDOM_SECRET_WORD,
     resave: true,
@@ -17,7 +18,7 @@ app.use(session({
 }));
 
 const oidc = new ExpressOIDC({
-    issuer: `${process.env.OKTA_ORG_URL}`,
+    issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
     client_id: process.env.OKTA_CLIENT_ID,
     client_secret: process.env.OKTA_CLIENT_SECRET,
     redirect_uri: process.env.REDIRECT_URL,
@@ -30,26 +31,68 @@ const oidc = new ExpressOIDC({
     }
 });
 
-// ExpressOIDC will attach handler for the /login and /authorization-code/callback routes
+// ExpressOIDC will attach handlers for the /login and /authorization-code/callback routes
 app.use(oidc.router);
+
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/home', (req, res) => {
-    res.send('<div><h1>Welcome!!</h1></div><a href="/login">Login</a>');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/admin', (req, res) => {
-    res.send('Admin page');
-});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/home.html'));
+ });
+ 
+ app.get('/admin', oidc.ensureAuthenticated(), (req, res) => {
+    res.sendFile(path.join(__dirname, './public/admin.html'));
+ });
 
 app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect('/home');
+  req.logout();
+  res.redirect('/home');
 });
 
 app.get('/', (req, res) => {
-    res.redirect('/home');
+  res.redirect('/home');
 });
 
-app.listen(port, () => console.log(`My Blog App is listening on port ${port}!`));
+const database = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite',
+    operatorsAliases: false,
+});
+
+const Post = database.define('posts', {
+    title: Sequelize.STRING,
+    content: Sequelize.TEXT,
+});
+
+finale.initialize({ app, sequelize: database });
+
+const PostResource = finale.resource({
+    model: Post,
+    endpoints: ['/posts', '/posts/:id'],
+});
+
+PostResource.all.auth(function (req, res, context) {
+    return new Promise(function (resolve, reject) {
+        if (!req.isAuthenticated()) {
+            res.status(401).send({ message: "Unauthorized" });
+            resolve(context.stop);
+        } else {
+            resolve(context.continue);
+        }
+    })
+});
+
+database.sync().then(() => {
+    oidc.on('ready', () => {
+        app.listen(port, () => console.log(`My Blog App listening on port ${port}!`))
+    });
+});
+
+oidc.on('error', err => {
+    // An error occurred while setting up OIDC
+    console.log("oidc error: ", err);
+});
